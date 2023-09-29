73d53058eea0c0f30523fd61b2032078c2c453bb
Jorge Flores <flores.jorgeluis@gmail.com>
2023-09-28 23:47:28 -0600

    se movio lo principal al archivo app.js

--- app.js
+++ app.js
@@ -1,20 +1,34 @@
-import express from 'express';
-import morgan from 'morgan';
-import authRoutes from './src/routes/auth_routes.js';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import morgan from "morgan";
+import authRoutes from "./src/routes/auth_routes.js";
+import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 //import postsRoutes from './src/routes/posts_routes.js';
 
+dotenv.config();
+const LOCAL_PORT = 3000;
+//require("dotenv").config();
+
+export const connectDB = async () => {
+  try {
+    // Usar la variable de entorno para la conexión a MongoDB
+    await mongoose.connect(process.env.MONGODB_URI, {});
+    console.log(">>> DB is connected");
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const app = express();
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
 
-
-app.use("/api",authRoutes);
+app.use("/api", authRoutes);
 
 //app.use("/api",postsRoutes);
 
-
-
-export default app;
+connectDB();
+app.listen(process.env.PORT || LOCAL_PORT);
+console.log("Server on port", process.env.PORT || LOCAL_PORT);
--- db.js
+++ db.js
@@ -1,15 +1,5 @@
 import mongoose from "mongoose";
 
-dotenv.config(); // Cargar las variables de entorno desde el archivo .env
+// Cargar las variables de entorno desde el archivo .env
 
 mongoose.set("strictQuery", false);
-
-export const connectDB = async () => {
-  try {
-    // Usar la variable de entorno para la conexión a MongoDB
-    await mongoose.connect(process.env.MONGODB_URI, {});
-    console.log(">>> DB is connected");
-  } catch (error) {
-    console.log(error);
-  }
-};
