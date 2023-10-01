00a7aa6e2d753e7e733dd1b13250ea4868419a55
Jorge Flores <flores.jorgeluis@gmail.com>
2023-10-01 12:16:43 -0600

    Se eliminaron archivos inecesarios.

--- db.js
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-
-// Cargar las variables de entorno desde el archivo .env
-
-mongoose.set("strictQuery", false);
--- index.js
+++
@@ -1,8 +0,0 @@
-import app from "./app.js";
-import { connectDB } from "./db.js";
-
-require("dotenv").config();
-
-connectDB();
-app.listen(process.env.PORT || 3000);
-console.log("Server on port", process.env.PORT || 3000);
